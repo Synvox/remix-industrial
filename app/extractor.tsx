1318776a1b9385bbeb17e1d9bd8cab425d38ec08
Ryan Allred <ryanallredxyz@gmail.com>
2023-01-11 22:29:20 -0700

    more wip

--- app/extractor.tsx
+++ app/extractor.tsx
@@ -18,9 +18,8 @@ export function getExtractor() {
     /**/ "<script>(()=>{" +
     /*  */ "let d=document,h=d.head,s=d.currentScript,p=s.parentElement;" +
     /*  */ "if(p!=h)" +
-    /*    */ 'while(let e=s.previousSibling,e.matches("[data-styled]"))' +
-    /*      */ "h.appendChild(e);" +
-    /*  */ "p.removeChild(s);" +
+    /*    */ "h.appendChild(s.previousSibling);" +
+    /*  */ "p.removeChild(s)" +
     /**/ "})()</script>";
 
   const transform = new Transform({
@@ -29,41 +28,16 @@ export function getExtractor() {
 
       if (firstRun) {
         firstRun = false;
-        const styleTags = sheet.getStyleTags();
+        const styleTag = sheet.getStyleTags();
         sheet.instance.clearTag();
-        result = result.replace(replaceString, styleTags);
+        result = result.replace(replaceString, styleTag);
       } else if (sheet.instance.toString().length) {
-        const closingTagRegex = /((<\/)\w+(>))/i;
-        const match = result.match(closingTagRegex);
-
-        if (match && match.index !== undefined) {
-          const endIndex = match.index + match[0].length;
-
-          const styleTag = sheet.getStyleTags();
-          sheet.instance.clearTag();
-
-          // Splice the new html into this chunk after a closing tag.
-          // Closing tags seem to work better than opening tags because
-          //   1. They have no attributes
-          //   2. You don't risk putting a tag as children of a special tag
-          //      like script, style, textarea, etc.
-          // There is still a risk of inserting into a script tag if the
-          // script tag includes a closing tag as a string. Splitting the
-          // tag like '</' + 'div>' may be the easiest solution.
-          result =
-            result.slice(0, endIndex) +
-            styleTag +
-            scriptTag +
-            result.slice(endIndex);
-        }
+        const styleTag = sheet.getStyleTags();
+        sheet.instance.clearTag();
+        result = styleTag + scriptTag + result;
       }
 
       callback(null, result);
-    },
-    flush(callback) {
-      if (!sheet.instance.toString().length) return callback(null, "");
-      const styleTag = sheet.getStyleTags();
-      callback(null, styleTag + scriptTag);
     },
   });
 
